28c61d2a533e4c857863866d81a8038602c3e3f9
Anna7682 <107514746+Anna7682@users.noreply.github.com>
2023-04-18 11:14:18 +0300

    Delete app.js

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-import Board from './Board';
-import GameFlow from './GameFlow';
-import Modal from './Modal';
-import SidePanel from './SidePanel';
-
-const gameBoard = new Board();
-const sidePanel = new SidePanel();
-const gameFlow = new GameFlow();
-Modal.createModal();
